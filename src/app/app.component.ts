e269f3216d598749002c025e84293ffbf6781309
mariokmxfx <mariokmxfx@gmail.com>
2025-06-10 16:04:09 -0700

    barra de generos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AdvertisementBannerComponent } from './components/advertisement-banner/advertisement-banner.component';
+import { OptionsBarComponent } from './components/options-bar/options-bar.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, 
             NavbarComponent,
-            AdvertisementBannerComponent],
+            AdvertisementBannerComponent,
+            OptionsBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/options-bar/options-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-options-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './options-bar.component.html',
+  styleUrl: './options-bar.component.css'
+})
+export class OptionsBarComponent {
+
+}
