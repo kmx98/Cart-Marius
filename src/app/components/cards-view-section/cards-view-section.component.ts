16043d478c72aaff46747bf73e166301cc361cea
mariokmxfx <mariokmxfx@gmail.com>
2025-06-21 13:48:44 -0700

    creacion de componente para card y arreglos a carrusel

--- src/app/components/cards-view-section/cards-view-section.component.ts
+++ src/app/components/cards-view-section/cards-view-section.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardComponent } from "../card/card.component";
 
 @Component({
   selector: 'app-cards-view-section',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './cards-view-section.component.html',
   styleUrl: './cards-view-section.component.css'
 })
