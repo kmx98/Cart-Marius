1bb70df9491e8efd91581f04699127184f758243
kmx98 <mariokmxfx@gmail.com>
2025-06-26 15:38:54 -0700

    footer (en desarrollo)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { AdvertisementBannerComponent } from './components/advertisement-banner/advertisement-banner.component';
 import { OptionsBarComponent } from './components/options-bar/options-bar.component';
 import { CardsViewSectionComponent } from './components/cards-view-section/cards-view-section.component';
+import { FooterViewComponent } from './components/footer-view/footer-view.component';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,8 @@ import { CardsViewSectionComponent } from './components/cards-view-section/cards
             NavbarComponent,
             AdvertisementBannerComponent,
             OptionsBarComponent,
-            CardsViewSectionComponent],
+            CardsViewSectionComponent,
+            FooterViewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
