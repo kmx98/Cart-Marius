a996b2dc426b99f909e1e33994335cb44cd39b2e
kmx98 <mariokmxfx@gmail.com>
2025-07-08 15:31:18 -0700

    Creacion de componente (o modulo) principal HOME

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { AdvertisementBannerComponent } from './components/advertisement-banner/advertisement-banner.component';
-import { OptionsBarComponent } from './components/options-bar/options-bar.component';
-import { CardsViewSectionComponent } from './components/cards-view-section/cards-view-section.component';
-import { FooterViewComponent } from './components/footer-view/footer-view.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, 
-            NavbarComponent,
-            AdvertisementBannerComponent,
-            OptionsBarComponent,
-            CardsViewSectionComponent,
-            FooterViewComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,12 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-import { routes } from './app.routes';
+ import { routes } from './app.routes';
+
+// const routes = [
+//   { path: '', component: HomeComponent },
+// ];
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
