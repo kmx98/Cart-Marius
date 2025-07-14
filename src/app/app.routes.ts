8348d0362f18e934737ee411b52fbf20466b103f
kmx98 <mariokmxfx@gmail.com>
2025-07-14 15:13:56 -0700

    Reordenamiento de componentes terminado (parece)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    // { path: '', component: CatalogComponent },
+    // { path: '', component: HomeComponent },
+    { path: '', component: CatalogComponent },
 ];
 
 
--- src/app/home/components/cards-view-section/cards-view-section.component.ts
+++ src/app/home/components/cards-view-section/cards-view-section.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../../shared/components/card/card.component';
-import { ScrollBarComponent } from '../../../components/scroll-bar/scroll-bar.component';
+import { ScrollBarComponent } from '../../../home/components/scroll-bar/scroll-bar.component';
+// import { ScrollBarComponent } from '../../../components/scroll-bar/scroll-bar.component';
 
 @Component({
   selector: 'app-cards-view-section',
