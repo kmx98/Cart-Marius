15c8b18a1ccffe9cf232d200746f467881cbbe8e
kmx98 <mariokmxfx@gmail.com>
2025-07-16 16:41:04 -0700

    Disenio de CATALOG estructurado (despues lo seguire trabajando)

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../shared/components/navbar/navbar.component';
 import { FooterViewComponent } from '../shared/components/footer-view/footer-view.component';
 import { FilterSidebarComponent } from './components/filter-sidebar/filter-sidebar.component';
+import { CardComponent } from '../shared/components/card/card.component';
 
 @Component({
     selector: 'app-catalog',
     standalone: true,
-    imports: [FilterSidebarComponent ,NavbarComponent, FooterViewComponent],
+    imports: [FilterSidebarComponent,
+            NavbarComponent, 
+            FooterViewComponent,
+            CardComponent],
     templateUrl: './catalog.component.html',
     styleUrl: './catalog.component.css'
 })
